a3fed8b52e973fdf4e52ecc5f9f385c75723e69e
Kouki <yamadakoki0124@icloud.com>
2022-03-14 15:00:58 +0900

    LoginModelの作成

--- Login_FaceID_2/ViewModel/LoginViewModel.swift
+++ Login_FaceID_2/ViewModel/LoginViewModel.swift
@@ -6,9 +6,34 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 class LoginViewModel: ObservableObject {
     
     @Published var email: String = ""
     @Published var password: String = ""
+    
+    //FaceIDのプロパティ
+    @AppStorage("use_faceID") var useFaceID = false
+    @AppStorage("use_face_email") var faceIDEmail = ""
+    @AppStorage("use_face_password") var faceIDPassword = ""
+    
+    //Log Status
+    @AppStorage("use_face_id") var logStatus = false
+    
+    //Firebase Login
+    //同期処理　async await
+    func loginUser() async throws {
+        
+        let _ = try await Auth.auth().signIn(withEmail: email, password: password)
+        
+        if useFaceID {
+            
+            //Storing for future face ID Login
+            faceIDEmail = email
+            faceIDPassword = password
+        }
+        
+        logStatus = true
+    }
 }
